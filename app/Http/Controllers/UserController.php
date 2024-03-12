3ca2fa409d71a139824253114d3a334ad87717d9
Airam Lus Diaz <airamluisdiaz629@gmail.com>
2024-03-12 19:31:02 +0000

    cambios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -33,7 +33,9 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $user = User::create($request->all());
+        
+        return response()->json($user);
     }
 
     /**
